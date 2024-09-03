71e9c6fbb7c33c7cfec7720d545ca152f0d7a48a
shaotj <shaotj@300624.cn>
2024-09-03 19:59:50 +0800

    整理readme文件

--- golang-interview/source/_static/js/baidutongji.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?1c00a5d8f392abda50bf926cb93939a1";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
